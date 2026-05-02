11f25c710b36a8ed7f4fce6afed902055244c922
Paulinho <paulohenriqgoes@gmail.com>
2026-05-02 12:22:47 -0300

    config para gh pages

---
+++ dist/assets/logDepthDeclaration-BfSj3c4L.js
@@ -0,0 +1,23 @@
+import{t as e}from"./shaderStore-jR05XatJ.js";var t=`sceneUboDeclaration`,n=`struct Scene {viewProjection : mat4x4<f32>,
+#ifdef MULTIVIEW
+viewProjectionR : mat4x4<f32>,
+#endif 
+view : mat4x4<f32>,
+projection : mat4x4<f32>,
+vEyePosition : vec4<f32>,};
+#define SCENE_UBO
+var<uniform> scene : Scene;
+`;e.IncludesShadersStoreWGSL[t]||(e.IncludesShadersStoreWGSL[t]=n);var r=`meshUboDeclaration`,i=`struct Mesh {world : mat4x4<f32>,
+visibility : f32,};var<uniform> mesh : Mesh;
+#define WORLD_UBO
+`;e.IncludesShadersStoreWGSL[r]||(e.IncludesShadersStoreWGSL[r]=i);var a=`defaultUboDeclaration`,o=`uniform diffuseLeftColor: vec4f;uniform diffuseRightColor: vec4f;uniform opacityParts: vec4f;uniform reflectionLeftColor: vec4f;uniform reflectionRightColor: vec4f;uniform refractionLeftColor: vec4f;uniform refractionRightColor: vec4f;uniform emissiveLeftColor: vec4f;uniform emissiveRightColor: vec4f;uniform vDiffuseInfos: vec2f;uniform vAmbientInfos: vec2f;uniform vOpacityInfos: vec2f;uniform vEmissiveInfos: vec2f;uniform vLightmapInfos: vec2f;uniform vSpecularInfos: vec2f;uniform vBumpInfos: vec3f;uniform diffuseMatrix: mat4x4f;uniform ambientMatrix: mat4x4f;uniform opacityMatrix: mat4x4f;uniform emissiveMatrix: mat4x4f;uniform lightmapMatrix: mat4x4f;uniform specularMatrix: mat4x4f;uniform bumpMatrix: mat4x4f;uniform vTangentSpaceParams: vec2f;uniform pointSize: f32;uniform alphaCutOff: f32;uniform refractionMatrix: mat4x4f;uniform vRefractionInfos: vec4f;uniform vRefractionPosition: vec3f;uniform vRefractionSize: vec3f;uniform vSpecularColor: vec4f;uniform vEmissiveColor: vec3f;uniform vDiffuseColor: vec4f;uniform vAmbientColor: vec3f;uniform cameraInfo: vec4f;uniform vReflectionInfos: vec2f;uniform reflectionMatrix: mat4x4f;uniform vReflectionPosition: vec3f;uniform vReflectionSize: vec3f;
+#define ADDITIONAL_UBO_DECLARATION
+#include<sceneUboDeclaration>
+#include<meshUboDeclaration>
+`;e.IncludesShadersStoreWGSL[a]||(e.IncludesShadersStoreWGSL[a]=o);var s=`mainUVVaryingDeclaration`,c=`#ifdef MAINUV{X}
+varying vMainUV{X}: vec2f;
+#endif
+`;e.IncludesShadersStoreWGSL[s]||(e.IncludesShadersStoreWGSL[s]=c);var l=`logDepthDeclaration`,u=`#ifdef LOGARITHMICDEPTH
+uniform logarithmicDepthConstant: f32;varying vFragmentDepth: f32;
+#endif
+`;e.IncludesShadersStoreWGSL[l]||(e.IncludesShadersStoreWGSL[l]=u);
